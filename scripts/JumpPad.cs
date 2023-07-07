c36af5f0184899dd213be38e264cb7fae9f59844
Mark Ross <krazkidd@gmail.com>
2023-07-07 07:56:47 -0700

    Add jump pad.

---
+++ scripts/JumpPad.cs
@@ -0,0 +1,32 @@
+using Godot;
+using System;
+
+public partial class JumpPad : Area2D
+{
+
+	[Export]
+	public float JumpMultiplier { get; set; } = 2.0f;
+
+	private AnimatedSprite2D animatedSprite2D;
+
+	public override void _Ready()
+	{
+		animatedSprite2D = GetNode<AnimatedSprite2D>("AnimatedSprite2D");
+
+		BodyEntered += (body) =>
+		{
+			if (body is Player player && player.Velocity.Y > 0.0f)
+			{
+				player.Velocity = new Vector2(0.0f, player.JumpVelocity * JumpMultiplier);
+
+				animatedSprite2D.Play("jump");
+			}
+		};
+	}
+
+	public override void _Process(double delta)
+	{
+
+	}
+
+}

3c50da18993ba9248b48564a325f7665d796c7d6
Mark Ross <krazkidd@gmail.com>
2023-07-09 16:07:02 -0700

    Damage player upon collision with a hitbox.

--- scripts/Hitbox.cs
+++ scripts/Hitbox.cs
@@ -1,5 +1,7 @@
 using Godot;
 using System;
+using System.Collections.Generic;
+using System.Linq;
 
 public partial class Hitbox : Area2D
 {
@@ -8,18 +10,43 @@ public partial class Hitbox : Area2D
 	public delegate void HitEventHandler(int damage, Vector2 direction);
 
 	[Export]
+	public int Damage { get; set; } = 10;
+	[Export]
 	public bool IsWeakSpot { get; set; } = false;
 	[Export]
 	public int CriticalDamageMultiplier { get; set; } = 2;
 
+	private Dictionary<Rid, Player> collidingBodies = new ();
+
 	public override void _Ready()
 	{
+		BodyEntered += (body) =>
+		{
+			if (body is Player player)
+			{
+				collidingBodies.Add(player.GetRid(), player);
 
+				player.HurtAsync(Damage, Vector2.Zero);
+			}
+		};
+		BodyExited += (body) =>
+		{
+			if (body is Player player)
+			{
+				collidingBodies.Remove(player.GetRid());
+			}
+		};
 	}
 
-	public override void _Process(double delta)
+	public override void _PhysicsProcess(double delta)
 	{
-
+		if (collidingBodies.Any())
+		{
+			foreach (KeyValuePair<Rid, Player> kvp in collidingBodies)
+			{
+				kvp.Value.HurtAsync(Damage, Vector2.Zero);
+			}
+		}
 	}
 
 	public void Hurt(int damage, Vector2 direction)

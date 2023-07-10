15291c1ddb947b9aba0e064cce2b6df05c94ba5b
Mark Ross <krazkidd@gmail.com>
2023-07-10 15:21:14 -0700

    Make regular barrels invincible. Play animation upon destruction of explosive barrel.

--- scripts/Barrel.cs
+++ scripts/Barrel.cs
@@ -7,21 +7,13 @@ public partial class Barrel : RigidBody2D
 	[Signal]
 	public delegate void HurtEventHandler(int damage);
 
-	[Export]
-	public int Health { get; set; } = 10;
-
-	#region Resourcesvirtual
+	#region Resources
 
 	private PackedScene shrapnelSplatter = ResourceLoader.Load<PackedScene>("res://scenes/shrapnel_splatter.tscn");
 
 	#endregion
 
-	public override void _Process(double delta)
-	{
-
-	}
-
-	protected async void AnimateInjuryAsync(int damage, Vector2 normal)
+	protected virtual async void AnimateInjuryAsync(int damage, Vector2 normal)
     {
         GpuParticles2D splatter = shrapnelSplatter.Instantiate<GpuParticles2D>();
 
@@ -34,32 +26,9 @@ public partial class Barrel : RigidBody2D
 		splatter.QueueFree();
     }
 
-	protected virtual void AnimateDeath()
-	{
-		//TODO
-	}
-
 	public virtual async void HurtAsync(int damage, Vector2 normal)
 	{
-		if (Health <= 0)
-		{
-			return;
-		}
-
-		Health = Math.Max(0, Health - damage);
-
-		EmitSignal(SignalName.Hurt, damage);
-
 		AnimateInjuryAsync(damage, normal);
-
-		if (Health <= 0)
-		{
-			AnimateDeath();
-
-			await ToSignal(GetTree().CreateTimer(5.0f), SceneTreeTimer.SignalName.Timeout);
-
-			QueueFree();
-		}
 	}
 
 }
--- scripts/ExplosiveBarrel.cs
+++ scripts/ExplosiveBarrel.cs
@@ -4,17 +4,43 @@ using System;
 public partial class ExplosiveBarrel : Barrel
 {
 
-	public override void _Ready()
-	{
-	}
+	[Export]
+	public int Health { get; set; } = 10;
 
-	public override void _Process(double delta)
-	{
-	}
+	#region Node references
 
-	protected override void AnimateDeath()
+	private CharacterAnimator characterAnimator;
+
+    #endregion
+
+    public override void _Ready()
+    {
+		characterAnimator = GetNode<CharacterAnimator>("CharacterAnimator");
+    }
+
+	protected virtual void AnimateDeath() => characterAnimator.AnimateDeath();
+
+	public override async void HurtAsync(int damage, Vector2 normal)
 	{
-		//TODO
+		if (Health <= 0)
+		{
+			return;
+		}
+
+		base.HurtAsync(damage, normal);
+
+		Health = Math.Max(0, Health - damage);
+
+		EmitSignal(SignalName.Hurt, damage);
+
+		if (Health <= 0)
+		{
+			AnimateDeath();
+
+			await ToSignal(GetTree().CreateTimer(5.0f), SceneTreeTimer.SignalName.Timeout);
+
+			QueueFree();
+		}
 	}
 
 }

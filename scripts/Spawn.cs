eca55cd10e9c61c4c2e38903d653a094bc103db6
Mark Ross <krazkidd@gmail.com>
2023-07-13 16:03:05 -0700

    Heal player while they are standing on spawn platform.

--- scripts/Spawn.cs
+++ scripts/Spawn.cs
@@ -1,5 +1,8 @@
 using Godot;
 using System;
+using System.Collections.Generic;
+using System.Linq;
+using MechJamIV;
 
 public partial class Spawn : Node2D
 {
@@ -10,11 +13,15 @@ public partial class Spawn : Node2D
 	[Export]
 	public bool IsWorldSpawn { get; set; } = false;
 
+	private Dictionary<Rid, Player> collidingBodies = new ();
+
 	#region Node references
 
 	public Marker2D SpawnPointMarker { get; set; }
 
+	private Timer healthGenTimer;
 	private Area2D area2D;
+	private CollisionShape2D collisionShape2D;
 	private GpuParticles2D gpuParticles2D;
 
 	#endregion
@@ -23,12 +30,22 @@ public partial class Spawn : Node2D
 	{
 		SpawnPointMarker = GetNode<Marker2D>("SpawnPoint");
 
+		healthGenTimer = GetNode<Timer>("HealthGenTimer");
+		healthGenTimer.Timeout += () => Generate();
+
 		area2D = GetNode<Area2D>("Area2D");
 		area2D.BodyEntered += (body) =>
 		{
 			if (body is Player player)
 			{
+				collidingBodies.Add(player.GetRid(), player);
+
 				gpuParticles2D.Visible = true;
+
+				if (healthGenTimer.IsStopped())
+				{
+					healthGenTimer.Start();
+				}
 
 				EmitSignal(SignalName.SpawnReached, player);
 			}
@@ -37,11 +54,27 @@ public partial class Spawn : Node2D
 		{
 			if (body is Player player)
 			{
-				gpuParticles2D.Visible = false;
+				collidingBodies.Remove(player.GetRid());
+
+				if (!collidingBodies.Any())
+				{
+					gpuParticles2D.Visible = false;
+
+					healthGenTimer.Stop();
+				}
 			}
 		};
 
+		collisionShape2D = GetNode<CollisionShape2D>("Area2D/CollisionShape2D");
 		gpuParticles2D = GetNode<GpuParticles2D>("GPUParticles2D");
+	}
+
+	private void Generate()
+	{
+		foreach (KeyValuePair<Rid, Player> kvp in collidingBodies)
+		{
+			kvp.Value.Heal(10);
+		}
 	}
 
 }

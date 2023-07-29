62598a48d92559f021bd5f69d87af694fe1a19ea
Mark Ross <krazkidd@gmail.com>
2023-07-29 08:39:29 -0700

    Wait a bit before pausing game upon player death.
    Let the death animation play.

--- scripts/levels/World.cs
+++ scripts/levels/World.cs
@@ -40,7 +40,13 @@ public partial class World : Node2D
 		InitDeathZones();
 
 		player.GlobalTransform = activeSpawn.SpawnPointMarker.GlobalTransform;
-		player.Killed += () => pauseScreen.PauseGame();
+		player.Killed += async () =>
+		{
+			//TODO ideally this should wait for the death animation to finish
+			await ToSignal(GetTree().CreateTimer(3.0f), SceneTreeTimer.SignalName.Timeout);
+
+			pauseScreen.PauseGame();
+		};
 
 		robot.GlobalTransform = player.RobotMarker.GlobalTransform;
 		robot.CharacterTracker.Track(player);

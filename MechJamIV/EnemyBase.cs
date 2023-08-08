dcab978ae83bcb2a99d60fcd88f438453da50338
Mark Ross <krazkidd@gmail.com>
2023-08-08 11:31:51 -0700

    Add default case to switch expression.

--- MechJamIV/EnemyBase.cs
+++ MechJamIV/EnemyBase.cs
@@ -109,6 +109,10 @@ namespace MechJamIV
                     ProcessAction_Attacking();
 
                     break;
+                default:
+                    // do nothing
+
+                    break;
             }
         }
 

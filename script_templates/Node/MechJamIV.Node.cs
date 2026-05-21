524c075c36653002d42f993c448cd7fdef54d2ab
Mark Ross <6730333+krazkidd@users.noreply.github.com>
2026-05-20 18:19:36 -0700

    Add script template with a project namespace.

---
+++ script_templates/Node/MechJamIV.Node.cs
@@ -0,0 +1,23 @@
+// meta-name: Node (MechJam IV)
+// meta-description: Base template for Node with default Godot cycle methods in the project namespace
+// meta-default: true
+// meta-space-indent: 4
+
+using _BINDINGS_NAMESPACE_;
+using System;
+
+namespace MechJamIV
+{
+    public partial class _CLASS_ : _BASE_
+    {
+        // Called when the node enters the scene tree for the first time.
+        public override void _Ready()
+        {
+        }
+
+        // Called every frame. 'delta' is the elapsed time since the previous frame.
+        public override void _Process(double delta)
+        {
+        }
+    }
+}
